ecbd5a2347cef9765c36004d9d2279621737d034
Estudiante <Estudiante@DESKTOP-3S9KSCS>
2023-05-04 10:36:59 +0200

    Ampliacion del mensaje

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//Esta es la clase principal de mi proyecto de lo que sea
+		//para la asignatura de ticII
+		
 	}
 
 }
